37814014347d99dd9b5cd146554140d9253b5ee9
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2023-06-21 19:13:48 +0600

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Client, Databases, ID } from 'appwrite';
 import { useState, useEffect } from 'react';
+import { BsFillCalendarEventFill } from 'react-icons/bs'
 
 const client = new Client();
 
@@ -13,39 +14,43 @@ client
 export default function Home() {
 
   const [blogPosts, setBlogPosts] = useState([])
-  useEffect(()=>{
+  useEffect(() => {
     document.title = "Home: The Hunting Coder"
     const databases = new Databases(client)
     let promise = databases.listDocuments(
       process.env.NEXT_PUBLIC_DATABASE,
       process.env.NEXT_PUBLIC_BLOG_COLLECTION
     )
-    promise.then(function(response){
-      setBlogPosts(response.documents)
-    }, function(error){
+    promise.then(function (response) {
+      setBlogPosts(response.documents.reverse())
+    }, function (error) {
       console.log(error)
     })
-  },[])
+  }, [])
 
   return (
     <div>
       <div className="max-w-6xl mx-auto p-4 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-      {blogPosts.map((post, index) => (
-        <div key={index} className="bg-zinc-200 p-4 rounded-lg shadow">
-          <Image
-            src={post.image}
-            alt={post.title}
-            width={300}
-            height={100}
-            className="w-full h-40 object-cover rounded-lg mb-4"
-          />
-          <h2 className="text-xl font-bold mb-2">{post.title}</h2>
-          <p className="text-gray-700 pb-3">{post.metadesc}...</p>
-          <Link href={`/blog/${post.slug}`} className="my-4 bg-sky-500 font-semibold rounded-lg px-3 py-1 text-white hover:bg-sky-600 duration-200">Read more
-          </Link>
-        </div>
-      ))}
-    </div>
+        {blogPosts.map((post, index) => (
+          <div key={index} className="bg-zinc-200 p-4 rounded-lg shadow">
+            <Image
+              src={post.image}
+              alt={post.title}
+              width={300}
+              height={100}
+              className="w-full h-40 object-cover rounded-lg mb-4"
+            />
+            <h2 className="text-xl font-bold mb-2">{post.title}</h2>
+            <div className='flex items-center pb-3 space-x-2'>
+              <BsFillCalendarEventFill className='text-zinc-800' />
+              <p>{post.$createdAt.split("T")[0]}</p>
+            </div>
+            <p className="text-gray-700 pb-2">{post.metadesc}...</p>
+            <Link href={`/blog/${post.slug}`} className="my-5 bg-sky-500 font-semibold rounded-lg px-3 py-1 text-white hover:bg-sky-600 duration-200">Read more
+            </Link>
+          </div>
+        ))}
+      </div>
 
     </div>
   )
