60b5b5984e2283cb6eacfd7a9f81b4260b950c11
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2023-06-25 15:35:41 +0600

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,23 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Hunting Coder',
+  description: 'A simple blog website made with NextJs 13, Tailwind CSS & Appwrite Database. @TAUSUR',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
