a6add270f1a73580ab7a337da29386ecffd3cb08
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2023-06-25 20:28:23 +0600

    Add files via upload

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,25 +1,126 @@
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 
-const Navbar = () => {
+const MobileNavbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="flex items-center justify-between p-4 bg-gray-800">
-      <div className="flex items-center">
-        <Link href="/" className="text-white text-xl font-bold">Hunting Coder
-        </Link>
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              onClick={toggleMenu}
+              type="button"
+              className="inline-flex items-center duration-300 justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className={`${isMenuOpen ? "hidden" : "block"} h-6 w-6`}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+              <svg
+                className={`${isMenuOpen ? "block" : "hidden"} h-6 w-6`}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center">
+              <Link href={"/"} className="text-white text-lg font-bold">
+                Hunting Coder
+              </Link>
+            </div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <Link
+                  href="/"
+                  className="text-gray-300 hover:bg-gray-700 duration-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Home
+                </Link>
+                <Link
+                  href="/about"
+                  className="text-gray-300 duration-500 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  About
+                </Link>
+                <Link
+                  href="/"
+                  className="text-gray-300 duration-500 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Works
+                </Link>
+                <Link
+                  href="/contact"
+                  className="text-gray-300 duration-500 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="hidden md:flex">
-        <Link href="/" className="text-gray-300 mx-4">Home
-        </Link>
-        <Link href="/about" className="text-gray-300 mx-4">About
-        </Link>
-        <Link href="/contact" className="text-gray-300 mx-4">Contact
-        </Link>
-      </div>
-      <div className="md:hidden">
-        {/* Add your mobile menu button here */}
+      <div
+        className={`${isMenuOpen ? "block" : "hidden"} sm:hidden duration-300`}
+      >
+        <div className="px-2 pt-2 pb-3 space-y-1">
+          <Link
+            href="/"
+            className="text-gray-300 duration-500 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Home
+          </Link>
+          <Link
+            href="/about"
+            className="text-gray-300 duration-500 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            About
+          </Link>
+          <Link
+            href="/"
+            className="text-gray-300 duration-500 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Works
+          </Link>
+          <Link
+            href="/contact"
+            className="text-gray-300 duration-500 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Contact
+          </Link>
+        </div>
       </div>
     </nav>
   );
 };
 
-export default Navbar;
+export default MobileNavbar;
