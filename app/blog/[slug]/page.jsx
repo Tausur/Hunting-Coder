8f5ca7d1b5c9ad947f4c9fe7b250be91ed45a7f8
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2023-06-26 20:39:54 +0600

    Add files via upload

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -34,7 +34,7 @@ export default function Blog({ params }) {
 
   return (
     <div>
-      <div className="max-w-4xl mx-auto p-4">
+      <div className="max-w-4xl mx-auto p-4 text-white">
         <h2 className="md:text-3xl text-xl font-bold mb-4 py-2">
           {blogPost?.title}
         </h2>
@@ -44,7 +44,7 @@ export default function Blog({ params }) {
           className="w-full rounded-lg mb-4"
         />
         <p
-          className="text-gray-700 mb-4"
+          className="text-gray-200 mb-4"
           dangerouslySetInnerHTML={{ __html: blogPost?.content }}
         ></p>
       </div>
